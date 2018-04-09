6de99c4767901355d058e7f1373d4537eeba9102
Oscar Lagatta <oscarlagatta@oscars-mbp.home>
2018-04-09 23:05:30 +0100

    adds ngrx configuration and displays orders data

--- src/app/documents/containers/documents/documents.component.ts
+++ src/app/documents/containers/documents/documents.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../services';
+import { Order } from '../../models/order.model';
 
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.scss']
+  styleUrls: ['./documents.component.scss'],
 })
 export class DocumentsComponent implements OnInit {
+  orders: Order[];
 
-  constructor() { }
+  constructor(private orderService: OrdersService) {}
 
   ngOnInit() {
+    this.orderService.getOrders().subscribe(orders => {
+      this.orders = orders;
+    });
   }
-
 }
