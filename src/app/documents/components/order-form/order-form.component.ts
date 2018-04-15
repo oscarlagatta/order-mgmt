055f2eaa1c6aa9f4e38d460c2029282b5d711751
Oscar Lagatta <oscarlagatta@oscars-mbp.home>
2018-04-15 21:17:15 +0100

    adds router state and entity composition

--- src/app/documents/components/order-form/order-form.component.ts
+++ src/app/documents/components/order-form/order-form.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Order } from '../../models/order.model';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
-  styleUrls: ['./order-form.component.scss']
+  styleUrls: ['./order-form.component.scss'],
 })
 export class OrderFormComponent implements OnInit {
+  @Input() order: Order;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
